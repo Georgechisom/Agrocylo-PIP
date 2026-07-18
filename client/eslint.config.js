89681ae963f106132f00e5aa938e35fc11171f25
dimka90 <dimkayilrit@gmail.com>
2026-07-18 21:24:42 +0100

    Format all config files and update scripts to cover root files

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -20,7 +20,10 @@ export default [
       ...jsxA11yPlugin.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
